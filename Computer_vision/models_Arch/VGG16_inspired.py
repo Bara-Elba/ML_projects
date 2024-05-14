f3e6bd7276a79e30e4f8c16ba3a938cdfb4d0e16
Bara-ELba <abdessamad.elbaraghi@edu.uca.ma>
2024-05-14 20:51:19 +0100

    grid search implementation for CNN model

--- Computer_vision/models_Arch/VGG16_inspired.py
+++ Computer_vision/models_Arch/VGG16_inspired.py
@@ -1,9 +1,16 @@
-import numpy as np 
-import os
+# import numpy as np 
+# import os
+import time
 import tensorflow as tf
 from tensorflow import keras
-from tensorflow.keras import models, layers
+# from tensorflow.keras import models, layers
 from keras.layers import Conv2D, MaxPooling2D, Flatten , Dense, Activation,Dropout
+from keras.wrappers.scikit_learn import KerasClassifier
+from keras.utils import to_categorical
+import sklearn
+from sklearn.metrics import accuracy_score
+from sklearn.model_selection import GridSearchCV
+import seaborn as sns
 
 
 
@@ -60,5 +67,91 @@ def mlp_model(input_shape=(512,512,3), output_shape=10 ,dropout_rate=0, dropout_
         )
     return model
 
+# define function to display the results of the grid search
+def display_cv_results(search_results):
+    print('Best score = {:.4f} using {}'.format(search_results.best_score_, search_results.best_params_))
+    means = search_results.cv_results_['mean_test_score']
+    stds = search_results.cv_results_['std_test_score']
+    params = search_results.cv_results_['params']
+    for mean, stdev, param in zip(means, stds, params):
+        print('mean test accuracy +/- std = {:.4f} +/- {:.4f} with: {}'.format(mean, stdev, param))
 
-input_shape=(32,32,3)
+
+
+
+
+
+if __name__ == '__main__':
+
+
+    # start= time.Time()
+    # print(start)
+
+    Cifar10=keras.datasets.cifar10 # Loading the dataset
+
+    (xtrain,ytrain),(xtest,ytest)= Cifar10.load_data()
+    class_names =['Airplane', 'Automobile', 'Bird', 'Cat', 'Deer', 'Dog', 'Frog', 'Horse', 'Ship', 'Truck']
+    print(class_names)
+
+    # Pixel value of the image falls between 0 to 255.
+
+    xtrain = xtrain/255 # So, we are scale the value between 0 to 1 before by deviding each value by 255
+    print(xtrain.shape)
+
+    xtest = xtest/255 # So, we are scale the value between 0 to 1 before by deviding each value by 255
+    print(xtest.shape)
+
+    ytrain=to_categorical(ytrain, num_classes=10)
+    ytest=to_categorical(ytest, num_classes=10)
+
+
+
+    input_shape=(32,32,3)
+    # create model
+    model = KerasClassifier(build_fn=mlp_model, verbose=1)
+    # define parameters and values for grid search
+    param_grid = {
+        'batch_size': [16, 32],
+        'epochs': [1],
+        'dropout_rate': [0.0, 0.50],
+    }
+    # grid = GridSearchCV(estimator=model, param_grid=param_grid, n_jobs=-1, cv=n_cv)
+    grid = GridSearchCV(estimator=model, param_grid=param_grid)
+    grid_result = grid.fit(xtrain, ytrain)  # fit the full dataset as we are using cross validation
+
+    # print out results
+    print('time for grid search = {:.0f} sec'.format(time()-start))
+    # display_cv_results(grid_result)
+
+    # reload best model
+    mlp = grid_result.best_estimator_
+
+    # retrain best model on the full training set
+    history = mlp.fit(
+        xtrain,
+        ytrain,
+        validation_data = (xtest, ytest),
+        epochs = 1
+    )
+    
+    # get prediction on validation dataset
+    y_pred = mlp.predict(xtest)
+    print('Accuracy on validation data = {:.4f}'.format(accuracy_score(ytest, y_pred)))
+
+    # plot accuracy on training and validation data
+    df_history = pd.DataFrame(history.history)
+    swarm_plot = sns.lineplot(data=df_history[['accuracy','val_accuracy']], palette="tab10", linewidth=2.5);
+    fig = swarm_plot.get_figure()
+    fig.savefig("out.png") 
+
+
+
+
+
+
+
+
+
+
+
+
